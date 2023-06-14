d9fbf3e76d5ef34e35b457c0762db81a3a71c284
khojashaheen <132402838+khojashaheen@users.noreply.github.com>
2023-06-14 15:15:43 -0400

    Uploading files

---
+++ mySketch.js
@@ -0,0 +1,385 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+let angleRotate = 0.0;
+	let i=0;
+	let j=0;
+	let increment=43;
+	
+	
+function draw() {
+
+	background("rgb(14,14,14)");
+	frameRate(100)
+
+	push(); // Start a new drawing state
+	translate(-300,-300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(0,-300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(300, -300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(600, -300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(900, -300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1200, -300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1500, -300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	
+	push(); // Start a new drawing state
+	translate(-300,0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(0,0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(300, 0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(600, 0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(900, 0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1200, 0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1500, 0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1800, 0);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(-300,300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(0,300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(300, 300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(600, 300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(900, 300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1200, 300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1500, 300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1800, 300);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(-300,600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(0,600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(300, 600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(600, 600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(900, 600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1200, 600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1500, 600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1800, 600);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(-300,900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(0,900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(300, 900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(600, 900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(900, 900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1200, 900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1500, 900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1800, 900);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(-300,1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(0,1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(300, 1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(600, 1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(900, 1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1200, 1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+
+	push(); // Start a new drawing state
+	translate(1500, 1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	push(); // Start a new drawing state
+	translate(1800, 1200);
+	rotate(radians(angleRotate));
+	createPattern(i,j,increment)
+	pop(); // Restore original state
+	
+	angleRotate += 1;
+}
+
+function createPattern(i,j,increment) {
+	createStar(i,j,increment)
+	createOctagon(i,j,increment)
+	createMiddleStar(i,j,increment)
+}
+
+function createStar(i,j,increment) {
+				//blue star
+				noStroke();
+
+				fill("rgb(136,6,100)")
+				beginShape();
+				vertex(i, j+3*increment);
+				vertex((i+2*increment),(j+2*increment));
+				vertex(i+3*increment,j);
+				vertex(i+4*increment,j+2*increment);
+				vertex(i+6*increment,j+3*increment);
+				vertex(i+4*increment,j+4*increment);
+				vertex(i+3*increment,j+6*increment)
+				vertex(i+2*increment,j+4*increment)
+				endShape(CLOSE);			
+	
+}
+
+
+function createOctagon(i,j, increment) {
+				noStroke();
+				fill("rgb(181,139,212)")
+				beginShape();
+				vertex(i,j+3*increment)
+				vertex(i+increment,j+increment)
+				vertex(i+3*increment,j)
+				vertex((i+2*increment),(j+2*increment));
+				vertex(i,j+3*increment)
+				vertex(i+increment,j+5*increment)
+				vertex(i+3*increment,j+6*increment)
+				vertex(i+2*increment,j+4*increment)
+				endShape(CLOSE);
+				beginShape();
+				vertex(i+6*increment,j+3*increment)
+				vertex(i+5*increment,j+5*increment)
+				vertex(i+3*increment,j+6*increment)
+				vertex(i+4*increment,j+4*increment)
+				vertex(i+6*increment,j+3*increment)
+				vertex(i+5*increment,j+increment)
+				vertex(i+3*increment,j)
+				vertex(i+4*increment,j+2*increment)
+				endShape(CLOSE);
+
+}
+
+
+function createMiddleStar(i,j,increment) {
+
+				fill("rgb(204,206,234)")
+				beginShape();
+				vertex(i, j+3*increment);
+				vertex((i+1*increment),(j+5*increment));
+				vertex(i+3*increment,j+6*increment);
+				vertex(i, j+6*increment);
+				endShape(CLOSE);
+	
+				beginShape();
+				vertex(i, j);
+				vertex(i+3*increment, j);
+				vertex((i+1*increment),(j+increment));
+				vertex(i, j+3*increment);
+				endShape(CLOSE);
+	
+				beginShape();
+				vertex(i+6*increment, j+3*increment);
+				vertex((i+5*increment),(j+5*increment));
+				vertex(i+3*increment,j+6*increment);
+				vertex(i+6*increment, j+6*increment);
+				endShape(CLOSE);
+				
+				beginShape();
+				vertex(i+6*increment, j);
+				vertex(i+3*increment, j);
+				vertex((i+5*increment),(j+increment));
+				vertex(i+6*increment, j+3*increment);
+				endShape(CLOSE);
+	
+
+}
